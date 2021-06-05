8387a81f34692099eec3f328698a3c43de827e6a
isotopeee <johnmichael.doroy@gmail.com>
2021-06-05 22:39:53 +0800

    feat: add activities model

---
+++ src/models/activities.ts
@@ -0,0 +1,60 @@
+import mongoose from 'mongoose';
+import { UsersSchema } from './users';
+
+const schema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  about: {
+    type: String,
+    required: true,
+  },
+  mechanics: {
+    type: String,
+    required: true,
+  },
+  rewardPoints: {
+    type: Number,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  organizer: {
+    name: {
+      type: String,
+      required: true,
+    },
+    avatarUrl: {
+      type: String,
+      required: true,
+    },
+  },
+  coverPhotoUrl: {
+    type: String,
+    required: true,
+  },
+  participants: [UsersSchema],
+  maxAttemptsCount: {
+    type: Number,
+    required: true,
+  },
+  startDate: {
+    type: Date,
+    required: true,
+  },
+  endDate: {
+    type: Date,
+    required: true,
+  },
+  scheduleTime: {
+    type: String,
+    required: true,
+  },
+});
+
+export const ActivitiesModel =
+  mongoose.models.activities || mongoose.model('activities', schema);
+export const ActivitiesSchema = schema;
