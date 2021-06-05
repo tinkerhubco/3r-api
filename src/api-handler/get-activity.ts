1e1d4734408b25616e890ceb4dcd7b21d095ea4e
isotopeee <johnmichael.doroy@gmail.com>
2021-06-05 23:56:38 +0800

    feat: implement get activity endpoint

---
+++ src/api-handler/get-activity.ts
@@ -0,0 +1,39 @@
+import { NextApiHandler } from 'next';
+
+import { database } from '../lib/database';
+import { ActivitiesModel } from '../models/activities';
+import { VouchersModel } from '../models/vouchers';
+
+export const getActivity: NextApiHandler = async (req, res) => {
+  await database.connect();
+
+  const userId = '60b762733a18d95422519836';
+
+  const { 'activity-id': activityId } = req.query;
+
+  const activity = await ActivitiesModel.findOne({
+    _id: activityId,
+  });
+
+  const hasJoined = !!activity.participants.find((id: any) => {
+    return id.equals(userId);
+  });
+
+  const earnedPoints = 10;
+  const attemptsCount = 1;
+  const participantsCount = activity.participants.length;
+  const vouchers = await VouchersModel.find({
+    activityId,
+  });
+
+  const activityWithComputedProperties = {
+    ...activity._doc,
+    hasJoined,
+    earnedPoints,
+    attemptsCount,
+    participantsCount,
+    vouchers,
+  };
+
+  return res.send(activityWithComputedProperties);
+};
